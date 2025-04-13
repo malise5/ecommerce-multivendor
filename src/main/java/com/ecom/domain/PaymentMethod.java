db05048a5ba5fdc27d3c08ef50abd11b93eabb7a
Halkano Malise <kudezx@gmail.com>
2025-04-13 11:26:46 +0300

    Add payment domain models and update PaymentOrder

---
+++ src/main/java/com/ecom/domain/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.ecom.domain;
+
+public enum PaymentMethod {
+    RAZORPAY,
+    PAYPAL,
+    STRIPE,
+    CASH
+}
--- src/main/java/com/ecom/model/PaymentOrder.java
+++ src/main/java/com/ecom/model/PaymentOrder.java
@@ -1,4 +1,35 @@
 package com.ecom.model;
 
+import com.ecom.domain.PaymentMethod;
+import com.ecom.domain.PaymentOrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PaymentOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private PaymentOrderStatus status = PaymentOrderStatus.PENDING;
+
+
+    private PaymentMethod paymentMethod;
+
+    private String paymentLinks;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany
+    private Set<Order> orders = new HashSet<>();
 }
