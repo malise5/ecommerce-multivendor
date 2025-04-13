8a685e24931c446ab86139ae2f4ab8c01a4caac2
Halkano Malise <kudezx@gmail.com>
2025-04-13 11:59:22 +0300

    feat: Initialize domain models and entities
    - Add core entity models (Deal, HomePage, HomePageSection, Seller)
    - Configure entity associations and collections
    - Add domain enums and DTOs

--- src/main/java/com/ecom/model/Deal.java
+++ src/main/java/com/ecom/model/Deal.java
@@ -1,10 +1,7 @@
 package com.ecom.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -18,4 +15,9 @@ public class Deal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Integer discount;
+
+    @OneToOne
+    private HomeCategory category;
 }
